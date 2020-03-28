4d6e43a158ea5f975473dd28b827474dfde52ebb
qnam <namdoanquoc.1998@gmail.com>
2020-03-28 05:00:03 +0000

    update version css

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -25,7 +25,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AnVui.vn -  Đi an về vui</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cabin:400,500,600,700&display=swap&subset=vietnamese">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v=1.0.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v=1.0.1">
 </head>
 <body>
     @php 
