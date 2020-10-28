c69200fcaa220a0b5b82d23799c2e628ed56a25b
QNam <namdoanquoc.1998@gmail.com>
2020-10-28 15:53:53 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,7 +31,7 @@ class AppServiceProvider extends ServiceProvider
         }
         
         $headerDefault = [
-            'metaTitle' => 'ANVUI.VN - Startup công nghệ số hóa ngành vận tải hành khác',
+            'metaTitle' => 'ANVUI.VN - Startup công nghệ số hóa ngành vận tải hành khách',
             'metaDesc' => 'AN VUI là đơn vị Công nghệ đầu tiên tại Việt Nam xây dựng hệ thống phần mềm tổng thể cho ngành vận tải hành khách. Nhằm giúp các nhà vận tải chủ động trong quản lý, nâng cao năng lực cạnh tranh từ đó giữ vững thị phần và mở rộng quy mô.',
             'metaKeyword' => 'vận tải,phần mềm,phần mềm nhà xe',
             'img' => asset('imgs/img-home2.png')
